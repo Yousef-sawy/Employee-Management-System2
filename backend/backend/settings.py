40e82c1efe70020398b2153f9bdacdfbf869e0da
yousef sawy <yousefsawy2@gmail.com>
2025-02-25 17:14:01 +0200

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'Users',
     'Company',
     'Departments',

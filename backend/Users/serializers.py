329f19dbf09dbd5e4a43981bff5c1258219f1d07
yousef sawy <yousefsawy2@gmail.com>
2025-02-25 16:33:15 +0200

    user-email val

--- backend/Users/serializers.py
+++ backend/Users/serializers.py
@@ -5,3 +5,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'role']
+    
+    def validate_email(self, value):
+        """Ensure email is unique"""
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
